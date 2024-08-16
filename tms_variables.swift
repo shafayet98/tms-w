56f2dde100bdf8861c09834fcf25fcd601f2b8c0
shafayet <shafayet.islam.61@gmail.com>
2024-08-17 05:39:55 +0930

    initial commit

---
+++ tms_variables.swift
@@ -0,0 +1,15 @@
+//
+//  tms_variables.swift
+//  
+//
+//  Created by Shafayet Ul Islam on 16/8/2024.
+//
+
+import Foundation
+
+// let are immutable which means it cannot be changed later or re-assigned later
+
+
+
+let name = "Audrie"
+print("My name is \(name)")
